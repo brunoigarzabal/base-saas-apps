3a0a8d95809dd3ea6c17b416060fbb83097476f4
Bruno Igarzabal <brunoigarzabal@outlook.com>
2026-01-20 20:43:43 -0300

    feat: add create user router

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -3,6 +3,10 @@ import { ZodTypeProvider } from 'fastify-type-provider-zod'
 
 import { z } from 'zod'
 
+import argon2 from 'argon2'
+
+import { prisma } from '@/lib/prisma'
+
 export function createAccountRoute(app: FastifyInstance) {
   app.withTypeProvider<ZodTypeProvider>().post(
     '/users',
@@ -16,7 +20,31 @@ export function createAccountRoute(app: FastifyInstance) {
       },
     },
     async (request, reply) => {
-      return 'User created!'
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply
+          .status(400)
+          .send({ message: 'User with same email already exists' })
+      }
+
+      const passwordHash = await argon2.hash(password)
+
+      const user = await prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send({ user })
     }
   )
 }
